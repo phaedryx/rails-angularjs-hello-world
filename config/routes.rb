b02eaafa030cdd1819f33554fafc4e2073bf7d1c
Tad Thorley <phaedryx@gmail.com>
2013-04-12 23:44:03 -0600

    add goodbye controller and some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 RailsAngularjsHelloWorld::Application.routes.draw do
   get "hello/index"
+  get "hello" => "hello#index"
+  get "goodbye/index"
+  get "goodbye" => "goodbye#index"
+
   root to: "hello#index"
 end
