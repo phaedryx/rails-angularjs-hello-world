beea96a1a0be68d9ebbd79c66d58d04b40a627e9
Tad Thorley <phaedryx@gmail.com>
2013-04-12 23:13:15 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsAngularjsHelloWorld::Application.config.secret_token = '4693d9ecf7fa28d0230070100754856a5526ae90528db56bcc702365d6ec9fc9bc7c19617f22cdba7e9178d465c9f6499a7fa239bc2ce1536d448d417c5cfdd5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RailsAngularjsHelloWorld::Application.config.session_store :cookie_store, key: '_rails-angularjs-hello-world_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RailsAngularjsHelloWorld::Application.config.session_store :active_record_store
